884fdf00bbd6bdab3d27103a96acb174d92610a3
Maxim Apasov <m.apasov2014@yandex.ru>
2025-06-12 19:16:25 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
-#BEFORE START DOWNLOAD FASTAPI, CTRANFORMERS, SENTANCE_TRANSFORMERS
-from fastapi import FastAPI, Body
+import torch
+from dotenv import load_dotenv
+import os
+from fastapi import FastAPI, Body, Depends, HTTPException, status
+from fastapi.security import APIKeyHeader
 from sentence_transformers import SentenceTransformer, util
-#from ctransformers import AutoModelForCausalLM
-
-"""llm = AutoModelForCausalLM.from_pretrained("TheBloke/Mistral-7B-Instruct-v0.1-GGUF", 
-                                           model_file="mistral-7b-instruct-v0.1.Q4_K_M.gguf", 
-                                           model_type="mistral", 
-                                           gpu_layers=25, 
-                                           threads=10, 
-                                           batch_size=1,
-                                           stream=False)"""
-
-SentTrans = SentenceTransformer("all-MiniLM-L6-v2")
 
 app = FastAPI()
 
-"""@app.post("/ask_question")
-async def ask_question(prompt:str = Body(embed=True), 
-                 temperature: float = Body(embed=True),
-                 max_tokens: int = Body(embed=True)):
-    new_prompt = "[INST]"+prompt+"[/INST]"
-    return(llm(new_prompt, temperature=temperature, max_new_tokens=max_tokens))"""
+load_dotenv()
+
+pwd = os.getenv("GAME_ENGINE")
+
+API_KEYS = {
+    "GameEngine": pwd
+}
+
+api_key_header = APIKeyHeader(name="X-API-Key")
+
+def get_api_key(api_key: str = Depends(api_key_header)):
+    if api_key not in API_KEYS.values():
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Token"
+        )
+    return api_key
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+#print(f"Используется устройство: {device}")
+SentTrans = SentenceTransformer("sberbank-ai/sbert_large_nlu_ru").to(device)
 
 @app.post("/compare_answers")
-async def compare_answers(CorrectAnswer:str = Body(embed=True), UserAnswer:str = Body(embed=True)):
-    emb1 = SentTrans.encode(CorrectAnswer, convert_to_tensor=True)
-    emb2 = SentTrans.encode(UserAnswer, convert_to_tensor=True)
-    similarity = util.cos_sim(emb1, emb2).item()
-    return round(similarity * 5)
+async def compare_answers(CorrectAnswer:str = Body(embed=True), 
+                          UserAnswer:str = Body(embed=True),
+                          api_key:str = Depends(get_api_key)):
+
+    emb1 = SentTrans.encode(CorrectAnswer, convert_to_tensor=True, device=device)
+    emb2 = SentTrans.encode(UserAnswer, convert_to_tensor=True, device=device)
+    result = util.cos_sim(emb1, emb2).item()
+
+    return round(result * 5)
